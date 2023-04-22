f2fa7ef865496134407188dc319a1418e71f16cd
DY-JS <delo1604@ukr.net>
2023-04-22 11:41:46 +0200

    add task 08

---
+++ freecodecamp/task-08.js
@@ -0,0 +1,11 @@
+// Write variable names in JavaScript in camelCase. In camelCase, multi-word variable names have the first word in lowercase and the first letter of each subsequent word is capitalized.
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+var studlyCapVar = 10;
+var properCamelCase = 'A String';
+var titleCaseOver = 9000;
