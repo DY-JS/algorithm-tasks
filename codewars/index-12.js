057dc526558be9ab970c95428f06c393a56b8423
DY-JS <delo1604@ukr.net>
2023-04-23 22:32:28 +0200

    Add task

--- codewars/index-12.js
+++ codewars/index-12.js
@@ -1,7 +1,7 @@
-function findOutlier(integers) {
-  // You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
-  // Вам дан массив (который будет иметь длину не менее 3, но может быть очень большим), содержащий целые числа. Массив либо полностью состоит из нечетных целых чисел, либо полностью состоит из четных целых чисел, за исключением одного целого числа N. Напишите метод, который принимает массив в качестве аргумента и возвращает этот «выброс» N.
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Вам дан массив (который будет иметь длину не менее 3, но может быть очень большим), содержащий целые числа. Массив либо полностью состоит из нечетных целых чисел, либо полностью состоит из четных целых чисел, за исключением одного целого числа N. Напишите метод, который принимает массив в качестве аргумента и возвращает этот «выброс» N.
 
+function findOutlier(integers) {
   const evenNums = integers.filter((num) => num % 2 == 0);
   const oddNums = integers.filter((num) => num % 2 !== 0);
   return evenNums.length == 1 ? evenNums[0] : oddNums[0];
