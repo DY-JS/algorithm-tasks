8265112d229db3434e50707c42b3a2f8ff9a6cc6
DY-JS <delo1604@ukr.net>
2023-04-23 22:58:40 +0200

    add task

---
+++ codewars/index-14.js
@@ -0,0 +1,18 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+function solution(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toUpperCase()) {
+      result += ' ' + str[i];
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
