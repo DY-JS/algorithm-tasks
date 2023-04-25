1a94ba519f63a44f743f37139aaf040b223e7820
DY-JS <delo1604@ukr.net>
2023-04-25 20:52:22 +0200

    add fix

--- codewars/index-17.js
+++ codewars/index-17.js
@@ -6,22 +6,22 @@
 // [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
 // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
-function sortArray(array) {
-  const odds = array.filter((num) => num % 2 === 1);
-  odds.sort((a, b) => a - b);
-
-  let idx = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 === 1) {
-      array[i] = odds[idx];
-      idx++;
-    }
-  }
-
-  return array;
-}
-
 // function sortArray(array) {
-//    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
-//    return array.map((x) => x % 2 ? odd.shift() : x);
+//   const odds = array.filter((num) => num % 2 === 1);
+//   odds.sort((a, b) => a - b);
+
+//   let idx = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 1) {
+//       array[i] = odds[idx];
+//       idx++;
+//     }
+//   }
+
+//   return array;
 // }
+
+function sortArray(array) {
+  const odd = array.filter((x) => x % 2).sort((a, b) => a - b);
+  return array.map((x) => (x % 2 ? odd.shift() : x));
+}
