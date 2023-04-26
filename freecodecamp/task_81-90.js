eaf7607a86cc14f24d2341f28b804f8fb1624a8c
DY-JS <delo1604@ukr.net>
2023-04-26 20:55:51 +0200

    add

--- freecodecamp/task_81-90.js
+++ freecodecamp/task_81-90.js
@@ -92,3 +92,27 @@ function golfScore(par, strokes) {
     return names[6];
   }
 }
+
+//87
+function caseInSwitch(val) {
+  let answer = '';
+  switch (val) {
+    case 1:
+      answer = 'alpha';
+      break;
+    case 2:
+      answer = 'beta';
+      break;
+    case 3:
+      answer = 'gamma';
+      break;
+    case 4:
+      answer = 'delta';
+      break;
+    default:
+      answer = 'Unknown value';
+  }
+  return answer;
+}
+
+//88
