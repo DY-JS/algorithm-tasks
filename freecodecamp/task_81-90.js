5ba7fc84bc2d49a16e03dab16466b07f4acb921d
DY-JS <delo1604@ukr.net>
2023-04-26 18:24:11 +0200

    add task

---
+++ freecodecamp/task_81-90.js
@@ -0,0 +1,9 @@
+//81
+function testLogicalOr(val) {
+  // Only change code below this line
+  if (val < 10 || val > 20) {
+    return 'Outside';
+  }
+  // Only change code above this line
+  return 'Inside';
+}
