9e1a0989ba9e5f84aec2d931425fd31f5ad14e14
DY-JS <delo1604@ukr.net>
2023-04-26 20:05:12 +0200

    add

--- freecodecamp/task_81-90.js
+++ freecodecamp/task_81-90.js
@@ -44,3 +44,22 @@ function orderMyLogic(val) {
     return 'Greater than or equal to 10';
   }
 }
+
+//85
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return 'Tiny';
+  } else if (num < 10) {
+    return 'Small';
+  } else if (num < 15) {
+    return 'Medium';
+  } else if (num < 20) {
+    return 'Large';
+  } else {
+    return 'Huge';
+  }
+
+  return 'Change Me';
+  // Only change code above this line
+}
