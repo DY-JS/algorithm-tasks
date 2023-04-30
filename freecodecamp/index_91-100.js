af2f6b408c3b28002681ec625379154d3c06e389
DY-JS <delo1604@ukr.net>
2023-04-30 14:42:11 +0200

    add 91

---
+++ freecodecamp/index_91-100.js
@@ -0,0 +1,15 @@
+//91
+function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  // Only change code above this line
+}
+
+////92;
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0 || b < 0) return;
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
