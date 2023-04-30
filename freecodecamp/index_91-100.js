cde9b1bc2ced2b03110c639a766823f774206897
DY-JS <delo1604@ukr.net>
2023-04-30 14:58:06 +0200

    add

--- freecodecamp/index_91-100.js
+++ freecodecamp/index_91-100.js
@@ -53,3 +53,39 @@ const myDog = {
   friends: [],
   // Only change code above this line
 };
+
+//95
+// Setup
+const testObj = {
+  hat: 'ballcap',
+  shirt: 'jersey',
+  shoes: 'cleats',
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
+
+//96
+// const testObj = {
+//   "an entree": "hamburger",
+//   "my side": "veggies",
+//   "the drink": "water"
+// };
+
+// Only change code below this line
+// const entreeValue = testObj["an entree"];   // Change this line
+// const drinkValue = testObj["the drink"];
+
+//97
+// const testObj = {
+//   12: "Namath",
+//   16: "Montana",
+//   19: "Unitas"
+// };
+
+// // Only change code below this line
+// const playerNumber = 16;  // Change this line
+// const player = testObj[playerNumber];   // Change this line
+
+//98
