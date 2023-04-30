7073713c100be616f6fcf1fed0d1886e8a1a71a3
DY-JS <delo1604@ukr.net>
2023-04-30 14:46:06 +0200

    add task

--- freecodecamp/index_91-100.js
+++ freecodecamp/index_91-100.js
@@ -13,3 +13,35 @@ function abTest(a, b) {
 
   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
+
+//93
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count--;
+      break;
+    default:
+      break;
+  }
+
+  if (count > 0) {
+    return count + ' Bet';
+  } else {
+    return count + ' Hold';
+  }
+}
+
+//94
